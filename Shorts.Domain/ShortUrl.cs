5a0f972cc4f15e6da5eeb68f29602dbf9d7ed5ed
odin <chudishko@gmail.com>
2016-06-30 17:42:52 +0300

    Added model for shortened URLs

---
+++ Shorts.Domain/ShortUrl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shorts.Domain
+{
+    public class ShortUrl
+    {
+        public long ShortUrlId { get; set; }
+        public string Url { get; set; }
+        public string Short { get; set; }
+        public int Clicks { get; set; } = 0;
+
+        private ShortUrl() { }
+
+        internal ShortUrl(string url)
+        {
+            this.Url = url;
+        }
+    }
+}
