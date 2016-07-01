8ff56fb738806a2a5405decd39e4f3edbc2362fb
odin <chudishko@gmail.com>
2016-07-01 09:40:32 +0300

    Renamed tests to match controller name

--- Shorts.Tests/Controllers/UrlControllerTest.cs
+++ Shorts.Tests/Controllers/UrlControllerTest.cs
@@ -18,7 +18,7 @@ using System.Threading;
 namespace Shorts.Tests.Controllers
 {
     [TestClass]
-    public class ValuesControllerTest
+    public class UrlControllerTest
     {
         private ShortsContext context;
         private UrlController controller;
